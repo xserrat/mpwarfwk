6dc44113843177b6752a658bbe3a4181daed66fe
Xavier Serrat Bordas <xavier.serrat22@gmail.com>
2015-03-11 21:37:06 +0100

    Modified Routing class in order to get routing configuration to load routes from config/routes.json

--- src/Mpwarfwk/Component/Routing.php
+++ src/Mpwarfwk/Component/Routing.php
@@ -5,16 +5,21 @@ namespace Mpwarfwk\Component;
 
 class Routing {
 
+    private $configRoutes;
+
     public function __construct(){
-        //TODO: Include config routes
-        echo "Routing<br>";
+        $pathConfigRoutes = $_SERVER['DOCUMENT_ROOT'] . "../config/routes.json";
+        $this->configRoutes = json_decode($pathConfigRoutes, true);
     }
 
     public function getRouteController($requestUri){
         //TODO: Return controller namespace of the request.
-        echo "Request URI:$requestUri<br>";
-        echo "REGEX RESULT: ".preg_replace("/.(.+)/", "\\1", $requestUri);
-        exit;
-        return "\Controllers\HomeController";
+        $route = preg_replace("/.(.+)/", "\\1", $requestUri);
+
+        if(!in_array($route, $this->configRoutes)){
+            return false;
+        }
+        $controllerNamespace = $this->configRoutes[$route];
+        return $controllerNamespace;
     }
 }
