082b450b249aa6a766c7eceb548cda12cb4e6aa0
Xavier Serrat Bordas <xavier.serrat22@gmail.com>
2015-03-22 13:08:30 +0100

    Added Container Injection and changed general config format to yaml

--- src/Mpwarfwk/Component/Bootstrap.php
+++ src/Mpwarfwk/Component/Bootstrap.php
@@ -6,19 +6,24 @@ use Mpwarfwk\Component\Http\Request,
     Mpwarfwk\Component\Session\Session,
     Mpwarfwk\Component\Routing\Route,
     Mpwarfwk\Component\Routing\Routing;
+use Mpwarfwk\Controller\ControllerAbstract;
+use Mpwarfwk\FileParser\YamlFileParser;
+use Mpwarfwk\Component\Container\Container;
 
 class Bootstrap {
 
-    const CONFIG_FILE_NAME = "config.json";
+    const CONFIG_FILE_NAME = "config.yaml";
     const PROD_ENVIRONMENT = 'PROD';
     const DEV_ENVIRONMENT = 'DEV';
 
     private $routing;
     private $request;
+    private $container;
 
     public function __construct(){
-        $this->routing = new Routing();
         $this->request = new Request(new Session());
+        $this->routing = new Routing();
+        $this->container = new Container();
     }
 
     public function run(){
@@ -34,20 +39,32 @@ class Bootstrap {
     private function executeController(Route $route){
         $controllerNamespace = $route->getControllerNamespace();
         $controller = new $controllerNamespace();
+
+        //Check if controller has ControllerAbstract extended to set container object
+        $controller = $this->injectContainer($controller);
+
         $response = $controller->{$route->getAction()}($this->request);
         return $response;
     }
 
+    private function injectContainer($controller){
+        if(get_parent_class($controller) === 'Mpwarfwk\Controller\ControllerAbstract'){
+            $controller->setContainer($this->container);
+        }
+        return $controller;
+    }
+
     public static function getRootApplicationPath(){
-        return preg_replace('/(.+)\/.+/', '\\1', $_SERVER['DOCUMENT_ROOT']);
+        return preg_replace('/(.+)\/.+/', '\\1', Request::$documentRoot);
     }
 
     public static function getApplicationConfig(){
-        return json_decode(file_get_contents(static::getRootApplicationPath().'/config/'.self::CONFIG_FILE_NAME), true);
+        $fileParser = new YamlFileParser(static::getRootApplicationPath().'/config/'.self::CONFIG_FILE_NAME);
+        return $fileParser->getFileData();
     }
 
     public static function getEnvironment(){
-        $entryScript = preg_replace('/.+\/(.+).php/', '\\1', $_SERVER['SCRIPT_FILENAME']);
+        $entryScript = preg_replace('/.+\/(.+).php/', '\\1', Request::$scriptFilename);
         if($entryScript === 'index'){
             return self::PROD_ENVIRONMENT;
         } elseif($entryScript === 'index_dev'){
