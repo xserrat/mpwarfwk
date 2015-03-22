b4b7cf0f7c58a030818eae91094f5b5216d11491
Xavier Serrat Bordas <xavier.serrat22@gmail.com>
2015-03-22 16:42:29 +0100

    Added modifications for possible pdo service

--- src/Mpwarfwk/Component/Db/PDODatabase.php
+++ src/Mpwarfwk/Component/Db/PDODatabase.php
@@ -8,7 +8,13 @@ class PDODatabase implements DatabaseInterface{
 
     private $pdoConnection;
 
-    public function __construct($host, $dbName, $username, $password){
+    public function __construct($host = null, $dbName = null, $username = null, $password = null){
+        $dsn = "mysql:host=$host;dbname=$dbName";
+        $this->pdoConnection = new PDO($dsn, $username, $password);
+        $this->pdoConnection->setAttribute( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC );
+    }
+
+    public function setConfig($host, $dbName, $username, $password){
         $dsn = "mysql:host=$host;dbname=$dbName";
         $this->pdoConnection = new PDO($dsn, $username, $password);
         $this->pdoConnection->setAttribute( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC );
