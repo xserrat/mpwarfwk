f32fd24b7f5a481c47f7a814b08007b1ae1a4805
Xavier Serrat Bordas <xavier.serrat22@gmail.com>
2015-03-22 12:57:53 +0100

    Added some shortcuts for render template with twig or smarty

--- src/Mpwarfwk/Controller/AbstractController.php
+++ src/Mpwarfwk/Controller/AbstractController.php
@@ -2,20 +2,39 @@
 
 namespace Mpwarfwk\Controller;
 
-use Mpwarfwk\Component\Request;
-use Mpwarfwk\Template\Template;
+use Mpwarfwk\Component\Http\Request;
+use Mpwarfwk\Component\Container\ContainerAbstract;
+use Mpwarfwk\Component\Http\Response;
+use Mpwarfwk\Component\Template\SmartyTemplate;
+use Mpwarfwk\Component\Template\TwigTemplate;
 
-class AbstractController {
+abstract class ControllerAbstract extends ContainerAbstract{
 
     public function forward($controller, $action, Request $request){}
-    public function redirect($url, $statusCode = 302){
+    public function redirect($url, $statusCode = Response::HTTP_TEMPORARY_REDIRECT){
 
     }
-    public function renderView($view, array $params){
 
-
+    public function get($id){
+        return $this->container->get($id);
     }
-    public function get($paramName){}
-    public function has($paramName){}
+
+    public function renderTwigView($template, array $params = array()){
+        /**
+         * @var $twig TwigTemplate
+         */
+        $twig = $this->container->get('templating-twig');
+        return $twig->render($template, $params);
+    }
+
+    public function renderSmartyView($template, array $params = array()){
+        /**
+         * @var $smarty SmartyTemplate
+         */
+        $smarty = $this->container->get('templating-smarty');
+        return $smarty->render($template, $params);
+    }
+    //public function getParam($paramName){}
+    public function hasParam($paramName){}
     public function getRequest(){}
 }
