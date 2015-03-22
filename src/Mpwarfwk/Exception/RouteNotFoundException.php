1274278ebcd4a7b3b21c6e2d4fd0a911423d9605
Xavier Serrat Bordas <xavier.serrat22@gmail.com>
2015-03-22 12:29:28 +0100

    Added some exceptions
    - Added exception for Route no found in routes.yaml
    - Added exception for Service not found in services.yaml

---
+++ src/Mpwarfwk/Exception/RouteNotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xserrat
+ * Date: 21/03/15
+ * Time: 22:52
+ */
+
+namespace Mpwarfwk\Exception;
+
+
+class RouteNotFoundException extends \Exception{
+
+}
