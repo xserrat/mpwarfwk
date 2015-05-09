050e7f64370f10e69c83af8d3a01eae67c0043c5
Xavier Serrat Bordas <xavier.serrat22@gmail.com>
2015-05-09 16:34:35 +0200

    Fixed bug at include array php from file

--- src/Mpwarfwk/FileParser/PhpFileParser.php
+++ src/Mpwarfwk/FileParser/PhpFileParser.php
@@ -6,6 +6,6 @@ namespace Mpwarfwk\FileParser;
 class PhpFileParser extends AbstractFileParser{
 
     public function __construct($filePath){
-        $this->configRoutes = include_once($filePath);
+        $this->configRoutes = include($filePath);
     }
 }
